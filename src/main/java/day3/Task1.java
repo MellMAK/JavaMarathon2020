834b4175381043f8276775ad5cb495db7072cd90
Roman Andriiv <roman.andriyiv@gmail.com>
2022-02-11 22:16:49 +0100

    add Task1 solution

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -1,7 +1,34 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        String city = sc.nextLine();
+        if (city.equals("")){
+            System.out.println("Input error");
+            return;
+        }
+        switch (city){
+            case "Warsaw":
+            case "Cracow":
+            case "Gdansk":
+                System.out.println("Poland");
+                break;
+            case "Kyiv":
+            case "Lviv":
+            case "Odessa":
+                System.out.println("Ukraine");
+                break;
+            case "Rome":
+            case "Milan":
+            case "Turin":
+                System.out.println("Italy");
+                break;
+            default:
+                System.out.println("Another country");
+                break;
+        }
     }
 }
