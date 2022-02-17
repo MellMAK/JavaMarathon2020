08490202d7007f4918f2b686d20824b48d3ce5e9
Roman Andriiv <roman.andriyiv@gmail.com>
2022-02-17 14:28:26 +0100

    refactor the code

--- src/main/java/day3/CityOfCountry.java
+++ src/main/java/day3/CityOfCountry.java
@@ -4,34 +4,36 @@ city name input.
 package day3;
 
 import java.util.Scanner;
-
+/*Implement a program that prints the name of the country to the console, taking on city name input.
+The program should run until it is entered the word "Stop".*/
 public class CityOfCountry {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String city = sc.nextLine();
-        if (city.equals("")){
-            System.out.println("Input error");
-            return;
-        }
-        switch (city){
-            case "Warsaw":
-            case "Cracow":
-            case "Gdansk":
-                System.out.println("Poland");
+        while (true) {
+            String city = sc.nextLine();
+            if (city.equals("Stop")){
                 break;
-            case "Kyiv":
-            case "Lviv":
-            case "Odessa":
-                System.out.println("Ukraine");
-                break;
-            case "Rome":
-            case "Milan":
-            case "Turin":
-                System.out.println("Italy");
-                break;
-            default:
-                System.out.println("Another country");
-                break;
+            }
+            switch (city){
+                case "Warsaw":
+                case "Cracow":
+                case "Gdansk":
+                    System.out.println("Poland");
+                    break;
+                case "Kyiv":
+                case "Lviv":
+                case "Odessa":
+                    System.out.println("Ukraine");
+                    break;
+                case "Rome":
+                case "Milan":
+                case "Turin":
+                    System.out.println("Italy");
+                    break;
+                default:
+                    System.out.println("Another country");
+                    break;
+            }
         }
     }
 }
