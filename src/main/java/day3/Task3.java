cbe14fe5d9aed71ab68c0caa88e2234333fdc8f2
Roman Andriiv <roman.andriyiv@gmail.com>
2022-02-17 14:29:36 +0100

    add Task3 solution

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,18 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i <5 ; i++) {
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+            if (b == 0) {
+                System.out.println("Division by 0");
+                continue;
+            }
+            System.out.println(a / b);
+        }
     }
 }
