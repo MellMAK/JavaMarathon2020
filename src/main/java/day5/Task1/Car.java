f71841e08cf9f7a63332310d5bf1af08c87ba746
Roman Andriiv <roman.andriyiv@gmail.com>
2022-02-21 16:34:03 +0100

    add Task1/Day5 solution

---
+++ src/main/java/day5/Task1/Car.java
@@ -0,0 +1,31 @@
+package day5.Task1;
+
+public class Car {
+    private int year;
+    private String color;
+    private String model;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
