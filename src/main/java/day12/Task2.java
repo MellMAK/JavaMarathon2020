22b61b5b439aba756c9df37ed8c028940eadee0f
RomanAndriiv <roman.andriyiv@gmail.com>
2022-04-08 16:09:46 +0200

    day12/task2 solution add

--- src/main/java/day12/Task2.java
+++ src/main/java/day12/Task2.java
@@ -1,7 +1,21 @@
 package day12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        List<Integer> arrList = new ArrayList();
+        for (int i = 0; i <= 30; i++) {
+            if (i % 2 == 0) {
+                arrList.add(i);
+            }
+        }
+        for (int i = 300; i <= 350; i++) {
+            if (i % 2 == 0) {
+                arrList.add(i);
+            }
+        }
+        System.out.println(arrList);
     }
 }
