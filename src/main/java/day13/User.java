0dbeeed810b8fd65e8bb4fbbf96f8d3586173028
RomanAndriiv <roman.andriyiv@gmail.com>
2022-04-19 13:19:52 +0200

    day13: User class implementation add

---
+++ src/main/java/day13/User.java
@@ -0,0 +1,50 @@
+package day13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String username;
+    private List<User> subscriptions;
+
+    public User(String username) {
+        this.username = username;
+        this.subscriptions = new ArrayList<>();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public List<User> getSubscriptions() {
+        return subscriptions;
+    }
+
+    public void subscribe(User user){
+        this.subscriptions.add(user);
+    }
+
+    public boolean isSubscribed(User user){
+        for (User currentUser : subscriptions) {
+            if (currentUser.getUsername().equals(user.getUsername())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isFriend(User user){
+        return this.isSubscribed(user)&& user.isSubscribed(this);
+    }
+
+    public void sendMassage(User user, String text){
+        // TODO
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                '}';
+    }
+}
